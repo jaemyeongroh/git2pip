571b0fef9087e37650bb6f0b7188707dc6055be2
rjm <jaemyeong.roh@snetsystems.co.kr>
2020-12-09 16:31:44 +0900

    test99

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup
 
-setup(name='cloudhub_lib',
-      version='0.0.3',
-      description='cloudhub pip install test',
-      url='https://github.com/jaemyeongroh/git2pip.git',
-      author='jaemyeongroh',
-      author_email='test@gmail.com',
-      license='jaemyeongroh',
-      packages=['cloudhub_lib==0.0.3'],
-      zip_safe=False,
-      install_requires=['salt==3001.1'])
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name=
+    'cloudhub_lib',  # 이 패키지를 설치/삭제할 때 사용할 이름을 의미한다. 이 이름과 import할 때 쓰이는 이름은 다르다.
+    version='0.0.3',
+    description='cloudhub pip install test',
+    long_description=long_description,
+    url='https://github.com/jaemyeongroh/git2pip.git',
+    author='jaemyeongroh',
+    author_email='test@gmail.com',
+    license='jaemyeongroh',
+    packages=['cloudhub_lib'],
+    zip_safe=False,
+    python_requires='>=3.6',
+    install_requires=requirements,
+)
